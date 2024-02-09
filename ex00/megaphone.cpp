801dbb90ad8fea3cacf86a0559449a82cdc1b84e
Tian SCHECROUN <schecroun.t@gmail.com>
2024-02-09 15:18:20 +0100

    yessir

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <cstring>
 
 int	main(int ac, char **av)
 {
-	if (ac < 2)
+	std::string s1;
 
+	if (ac < 2)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n", 1);
+	for (int x = 1; x < ac; x++)
+	{
+//		if (x != 1)
+//			putchar(' ');
+		s1 = av[x];
+		for (int y = 0; y < s1.size(); y++)
+			putchar(toupper(s1[y]));
+	}
+	putchar('\n');
+	return (1);
 }
