2ff4219c27ef7d8cf829a67aba3157a5e357ee1c
Tian SCHECROUN <schecroun.t@gmail.com>
2024-02-12 17:42:15 +0100

    debut struct des commandes

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,16 +2,36 @@
 #include "Contact.hpp"
 #include "includes.hpp"
 
+void	Add_command(std::string input){
+	
+	if (input == "ADD")
+		std::cout << "ADD command confirmed" << std::endl;
+}
+
+void	Search_command(std::string input){
+	
+	if (input == "SEARCH")
+		std::cout << "SEARCH command confirmed" << std::endl;
+}
+
+void	Exit_command(std::string input){
+	
+	if (input == "EXIT")
+		std::cout << "EXIT command confirmed" << std::endl;
+}
+
 int	main(void){
 
 	//Contact		contact[8];
 	Phonebook	book;
 	std::string	yo;
-	std::cout << "Enter a command : "<< std::endl;
-
-	std::getline(std::cin, yo);
-
+	while (true){
+		std::cout << "Enter a command : "<< std::endl;
+		std::getline(std::cin, yo);
+		Add_command(yo);
+		Search_command(yo);
+		Exit_command(yo);
+	}
 	std::cout << yo << std::endl;
-
 	return 0;
 }
